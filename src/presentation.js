e6c4ff762bfc915d07f266628140ce988904c9b4
Trevor Eyre <trevoreyre@gmail.com>
2018-11-02 11:13:09 -0600

    Added Prism CSS and JS files for code highlighting

--- src/presentation.js
+++ src/presentation.js
@@ -3,13 +3,8 @@ import React from 'react';
 
 // Import Spectacle Core tags
 import {
-  BlockQuote,
-  Cite,
   Deck,
   Heading,
-  ListItem,
-  List,
-  Quote,
   Slide,
   Text,
 } from 'spectacle';
@@ -52,15 +47,6 @@ export default class Presentation extends React.Component {
         progress={null}
         theme={theme}
       >
-        <CodeSlide
-          bgColor="secondary"
-          lang="jsx"
-          code={codeSample1}
-          ranges={[
-            { loc: [1, 2] },
-            { loc: [2, 6] }
-          ]}
-        />
 
         <Slide bgColor="secondary">
           <Heading size={1} fit caps lineHeight={1} textColor="primary">
@@ -71,48 +57,16 @@ export default class Presentation extends React.Component {
           </Text>
         </Slide>
 
-        <Slide bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
-        </Slide>
+        <CodeSlide
+          bgColor="secondary"
+          lang="jsx"
+          code={codeSample1}
+          ranges={[
+            { loc: [1, 2] },
+            { loc: [2, 6] }
+          ]}
+        />
 
-        <Slide bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-
-        <Slide bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
       </Deck>
     );
   }
