0563366f111a6230f2dfc69c90dd27ef2e2606a7
Trevor Eyre <trevoreyre@gmail.com>
2018-11-09 14:01:35 +0000

    Removed app-progress folder

--- app-progress/src/components/Grid.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Grid = ({ children }) => <div className="grid">{children}</div>
-
-export default Grid
--- app-progress/src/components/Main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Main = ({ children, ...props }) => (
-  <main className="main" {...props}>
-    {children}
-  </main>
-)
-
-export default Main
